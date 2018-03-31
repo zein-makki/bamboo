1c16017865dab9bf3c2f46434751cf2b4ab87143
Zein Makki <zeinsmakki@gmail.com>
2018-03-31 14:22:06 +0300

    PROJ-0001 Fix removed files

---
+++ BambooTests/Tests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BambooTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [TestCase]
+        public void Test1()
+        {
+
+        }
+
+        [TestCase]
+        public void Test2()
+        {
+
+        }
+
+        [TestCase]
+        public void Test3()
+        {
+
+        }
+    }
+}
